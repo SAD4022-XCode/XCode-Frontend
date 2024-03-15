d48c6a3365e19dd7cd640d57b0b5c762e6a917f8
Alibest3 <alibest3@yahoo.com>
2024-03-16 00:56:53 +0330

    connection between login and signup form changed

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -7,6 +7,10 @@ const Signup = () => {
   const [enteredRegisterPassword, setEnteredRegisterPassword] = useState("");
   const [enteredName, setEnteredName] = useState("");
   const [enteredBirthDate, setEnteredBirthDate] = useState("");
+  const [showLogin, setShowLogin] = useState(true);
+  const toggleForm = () => {
+    setShowLogin(!showLogin);
+  };
   const submitHandler = (event, action) => {
     event.preventDefault();
     let userData = "";
@@ -49,122 +53,126 @@ const Signup = () => {
   const birthDateHandler = (event) => {
     setEnteredBirthDate(event.target.value);
   };
+
   return (
     <form className="signin" onSubmit={submitHandler}>
-      <div id="stars"></div>
-      <div id="stars2"></div>
-      <div id="stars3"></div>
       <div className="section">
         <div className="container">
           <div className="row full-height justify-content-center">
             <div className="col-12 text-center align-self-center py-5">
               <div className="section pb-5 pt-5 pt-sm-2 text-center">
-                <h6 className="mb-0 pb-3">
-                  <span>Log In </span>
-                  <span>Sign Up</span>
-                </h6>
-                <input
-                  className="checkbox"
-                  type="checkbox"
-                  id="reg-log"
-                  name="reg-log"
-                />
-                <label for="reg-log"></label>
                 <div className="card-3d-wrap mx-auto">
                   <div className="card-3d-wrapper">
-                    <div className="card-front">
-                      <div className="center-wrap">
-                        <div className="section text-center">
-                          <h4 className="mb-4 pb-3">Log In</h4>
-                          <div className="form-group">
-                            <input
-                              type="email"
-                              className="form-style"
-                              placeholder="Email"
-                              value={enteredLoginEmail}
-                              onChange={loginEmailHandler}
-                            />
-                            <i className="input-icon uil uil-at"></i>
+                    {showLogin && (
+                      <div className="card-front">
+                        <div className="center-wrap">
+                          <div className="section text-center">
+                            <h4 className="mb-4 pb-3">Log In</h4>
+                            <div className="form-group">
+                              <input
+                                type="email"
+                                className="form-style"
+                                placeholder="Email"
+                                value={enteredLoginEmail}
+                                onChange={loginEmailHandler}
+                              />
+                              <i className="input-icon uil uil-at"></i>
+                            </div>
+                            <div className="form-group mt-2">
+                              <input
+                                type="password"
+                                className="form-style"
+                                placeholder="Password"
+                                value={enteredLoginPassword}
+                                onChange={loginPasswordHandler}
+                              />
+                              <i className="input-icon uil uil-lock-alt"></i>
+                            </div>
+                            <button
+                              type="submit"
+                              className="btn mt-4"
+                              onClick={(e) => submitHandler(e, "login")}
+                            >
+                              Login
+                            </button>
+                            <p className="message">
+                              Not registered?{" "}
+                              <a href="#" onClick={toggleForm}>
+                                Create an account
+                              </a>
+                            </p>
+
+                            <p className="mb-0 mt-4 text-center">
+                              <a className="link">Forgot your password?</a>
+                            </p>
                           </div>
-                          <div className="form-group mt-2">
-                            <input
-                              type="password"
-                              className="form-style"
-                              placeholder="Password"
-                              value={enteredLoginPassword}
-                              onChange={loginPasswordHandler}
-                            />
-                            <i className="input-icon uil uil-lock-alt"></i>
-                          </div>
-                          <button
-                            type="submit"
-                            className="btn mt-4"
-                            onClick={(e) => submitHandler(e, "login")}
-                          >
-                            Login
-                          </button>
-                          <p className="mb-0 mt-4 text-center">
-                            <a className="link">Forgot your password?</a>
-                          </p>
                         </div>
                       </div>
-                    </div>
-                    <div className="card-back">
-                      <div className="center-wrap">
-                        <div className="section text-center">
-                          <h4 className="mb-3 pb-3">Sign Up</h4>
-                          <div className="form-group">
-                            <input
-                              type="text"
-                              className="form-style"
-                              placeholder="Full Name"
-                              value={enteredName}
-                              onChange={nameHandler}
-                            />
-                            <i className="input-icon uil uil-user"></i>
+                    )}
+                    {!showLogin && (
+                      <div className="card-back">
+                        <div className="center-wrap">
+                          <div className="section text-center">
+                            <h4 className="mb-3 pb-3">Sign Up</h4>
+                            <div className="form-group">
+                              <input
+                                type="text"
+                                className="form-style"
+                                placeholder="Full Name"
+                                value={enteredName}
+                                onChange={nameHandler}
+                              />
+                              <i className="input-icon uil uil-user"></i>
+                            </div>
+                            <div className="form-group mt-2">
+                              <input
+                                type="email"
+                                className="form-style"
+                                placeholder="Email"
+                                value={enteredRegisterEmail}
+                                onChange={registerEmailHandler}
+                              />
+                              <i className="input-icon uil uil-at"></i>
+                            </div>
+                            <div className="form-group mt-2">
+                              <input
+                                type="password"
+                                className="form-style"
+                                placeholder="Password"
+                                value={enteredRegisterPassword}
+                                onChange={registerPasswordHandler}
+                              />
+                              <i className="input-icon uil uil-lock-alt"></i>
+                            </div>
+                            <div className="form-group mt-2">
+                              <input
+                                type="text"
+                                className="form-style"
+                                placeholder="Birthdate"
+                                onFocus={(e) => (e.target.type = "date")}
+                                onBlur={(e) => (e.target.type = "text")}
+                                value={enteredBirthDate}
+                                onChange={birthDateHandler}
+                              />
+                              <i className="input-icon uil uil-calendar-alt"></i>
+                            </div>
+                            <button
+                              type="submit"
+                              className="btn mt-4"
+                              onClick={(e) => submitHandler(e, "register")}
+                            >
+                              Register
+                            </button>
+                            <p className="message">
+                              Already registered?{" "}
+                              <a href="#" onClick={toggleForm}>
+                                Sign In
+                              </a>
+                            </p>
                           </div>
-                          <div className="form-group mt-2">
-                            <input
-                              type="email"
-                              className="form-style"
-                              placeholder="Email"
-                              value={enteredRegisterEmail}
-                              onChange={registerEmailHandler}
-                            />
-                            <i className="input-icon uil uil-at"></i>
-                          </div>
-                          <div className="form-group mt-2">
-                            <input
-                              type="password"
-                              className="form-style"
-                              placeholder="Password"
-                              value={enteredRegisterPassword}
-                              onChange={registerPasswordHandler}
-                            />
-                            <i className="input-icon uil uil-lock-alt"></i>
-                          </div>
-                          <div className="form-group mt-2">
-                            <input
-                              type="text"
-                              className="form-style"
-                              placeholder="Birthdate"
-                              onFocus={(e) => (e.target.type = "date")}
-                              onBlur={(e) => (e.target.type = "text")}
-                              value={enteredBirthDate}
-                              onChange={birthDateHandler}
-                            />
-                            <i className="input-icon uil uil-calendar-alt"></i>
-                          </div>
-                          <button
-                            type="submit"
-                            className="btn mt-4"
-                            onClick={(e) => submitHandler(e, "register")}
-                          >
-                            Register
-                          </button>
                         </div>
                       </div>
-                    </div>
+                    )}
                   </div>
                 </div>
               </div>
