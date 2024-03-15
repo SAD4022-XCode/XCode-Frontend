19f59774085f420c8f300fb654906b0792577af1
AliGh <aaghz1381@gmail.com>
2024-03-15 15:50:59 +0330

    feat(login-register): Added Google account login feature to enable users to log in with their Google accounts

---
+++ src/Components/Login-Register/google.js
@@ -0,0 +1,28 @@
+import {GoogleLogin} from 'react-google-login';
+
+const clientID = "191069690020-bfq8g99fjkeskb60o0rqjri7cecm6r9l.apps.googleusercontent.com";
+
+function Login(){
+    const onSuccess = (res) =>{
+        console.log("Login Success! Current User: ",res.profileObj);
+    }
+
+    const onFailure = (res) =>{
+        console.log("Login Failed! res: ",res);
+    }
+
+    return (
+        <div id="googleButton">
+            <GoogleLogin
+                clientId={clientID}
+                buttonText='Login with Google'
+                onSuccess={onSuccess}
+                onFailure={onFailure}
+                cookiePolicy={'single_host_origin'}
+                isSignedIn={true}            
+            />
+        </div>
+    )
+}
+
+export default Login;
