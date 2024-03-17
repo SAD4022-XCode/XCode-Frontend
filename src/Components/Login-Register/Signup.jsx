33c79b4f0c16f9e4e0c84961b2c54d4e46441393
AliGh <aaghz1381@gmail.com>
2024-03-17 22:42:28 +0330

    feat(login-register): Add input data validation before sending to backend

--- src/Components/Login-Register/Signup.jsx
+++ src/Components/Login-Register/Signup.jsx
@@ -3,11 +3,14 @@ import "./Signup.css";
 import GoogleLogin from "./google";
 import { gapi } from "gapi-script";
 import 'animate.css';
+// import Validation from "./validation";
+
 const clientID ="191069690020-bfq8g99fjkeskb60o0rqjri7cecm6r9l.apps.googleusercontent.com";
 
 let lastToggleFormTime = 0;
-
 const Signup = () => {
+  
+  //form input variables
   const [enteredLoginEmail, setEnteredLoginEmail] = useState("");
   const [enteredRegisterEmail, setEnteredRegisterEmail] = useState("");
   const [enteredLoginPassword, setEnteredLoginPassword] = useState("");
@@ -16,9 +19,27 @@ const Signup = () => {
   const [enteredName, setEnteredName] = useState("");
   const [enteredBirthDate, setEnteredBirthDate] = useState("");
   const [showLogin, setShowLogin] = useState(true);
-  const [showPassword, setShowPassword] = useState(false);
-  const [showPassword2, setShowPassword2] = useState(false);
 
+  //card height variables
+  const [autoHeight,setAutoHeight] = useState(450);
+  const [violationNumber,setViolationNumber] = useState(0);
+  //show password variables
+  const [showLoginPassword, setShowLoginPassword] = useState(false);
+  const [showRegisterPassword, setShowRegisterPassword] = useState(false);
+  const [showRegisterPassword2, setShowRegisterPassword2] = useState(false);
+  //validation variables
+  const [showViolations, setShowViolation] = useState(false);
+  const [loginEmailValidation, setLoginEmailValidation] = useState(false);
+  const [registerEmailValidation, setRegisterEmailValidation] = useState(false);
+  const [loginPasswordValidation, setLoginPasswordValidation] = useState(false);
+  const [loginPasswordValidationMsg, setLoginPasswordValidationMsg] = useState(false);
+
+  const [registerPasswordValidation, setRegisterPasswordValidation] = useState(false);
+  const [registerPasswordValidationMsg, setRegisterPasswordValidationMsg] = useState(false);
+  const [registerPasswordValidation2, setRegisterPasswordValidation2] = useState(false);
+  const [registerPasswordValidationMsg2, setRegisterPasswordValidationMsg2] = useState(false);
+  const [nameValidation,setNameValidation] = useState(false)
+  
   useEffect(() => {
     function start() {
       gapi.client.init({
@@ -29,32 +50,44 @@ const Signup = () => {
     gapi.load("client:auth2", start);
   });
   
-  
 
-  const togglePasswordVisibility = () => {
-    setShowPassword(!showPassword);
+  const toggleLoginPasswordVisibility = () => {
+    setShowLoginPassword(!showLoginPassword);
   };
-  const togglePasswordVisibility2 = () => {
-    setShowPassword2(!showPassword2);
+  const toggleRegisterPasswordVisibility = () => {
+    setShowRegisterPassword(!showRegisterPassword);
+  };
+  const toggleRegisterPasswordVisibility2 = () => {
+    setShowRegisterPassword2(!showRegisterPassword2);
   };
   const toggleForm = () => {
+    setShowViolation(false)
     
-    setShowPassword(false);
-    setShowPassword2(false);
+
+    setShowLoginPassword(false);
+    setShowRegisterPassword(false);
+    setShowRegisterPassword2(false);
+
 
     const currentTime = Date.now();
     if (currentTime - lastToggleFormTime < 700) return; 
     lastToggleFormTime = currentTime;
 
+    setViolationNumber(0);
+    showLogin === true ? setAutoHeight(500) : setAutoHeight(430);
+
     setShowLogin(!showLogin);
+
     const formWrapper = document.querySelector(".card-3d-wrap");
     if (formWrapper.classList.contains("animate__animated", "animate__fadeInDown", "animate__faster")) {
+      
       formWrapper.classList.remove("animate__animated", "animate__fadeInUp", "animate__faster");
       formWrapper.classList.add("animate__animated", "animate__fadeInDown", "animate__faster");
       setTimeout(() => {
         formWrapper.classList.remove("animate__animated", "animate__fadeInDown", "animate__faster");
       }, 600);
     } else {
+      
       formWrapper.classList.remove("animate__animated", "animate__fadeInDown", "animate__faster");
       formWrapper.classList.add("animate__animated", "animate__fadeInUp", "animate__faster");
       setTimeout(() => {
@@ -65,6 +98,8 @@ const Signup = () => {
   
 
   const submitHandler = (event, action) => {
+    setAutoHeight(autoHeight+violationNumber*7);
+    setShowViolation(true)
     event.preventDefault();
     let userData = "";
     if (action === "login") {
@@ -72,8 +107,14 @@ const Signup = () => {
         email: enteredLoginEmail,
         password: enteredLoginPassword,
       };
-      setEnteredLoginEmail("");
-      setEnteredLoginPassword("");
+      
+      if(loginEmailValidation && loginPasswordValidation){
+        setEnteredLoginEmail("");
+        setEnteredLoginPassword("");
+        //send data to back-end
+        //show snack bar
+      }
+ 
     } else {
       userData = {
         name: enteredName,
@@ -81,32 +122,190 @@ const Signup = () => {
         password: enteredRegisterPassword,
         birthDate: new Date(enteredBirthDate),
       };
-      setEnteredRegisterEmail("");
-      setEnteredRegisterPassword("");
-      setEnteredRegisterPassword2("");
-      setEnteredName("");
-      setEnteredBirthDate("");
+      if(registerEmailValidation && registerPasswordValidation && registerPasswordValidation2 && nameValidation){
+        setEnteredRegisterEmail("");
+        setEnteredRegisterPassword("");
+        setEnteredRegisterPassword2("");
+        setEnteredName("");
+        setEnteredBirthDate("");
+        //send data to back-end
+        //show snack bar
+      }
+      
     }
     console.log(userData);
   };
+
+  //Login validation
+  //--------------------------------------------------------------------------------------------------
   const loginEmailHandler = (event) => {
+    if(showViolations===true){
+      setAutoHeight(autoHeight-violationNumber*7);
+    }
+    setViolationNumber(0);
+    setShowViolation(false)
     setEnteredLoginEmail(event.target.value);
+    if (!String(event.target.value)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    )){
+      setLoginEmailValidation(false);
+    }
+    else{
+      setLoginEmailValidation(true);
+    }
+    if(loginEmailValidation===false){
+      setViolationNumber(violationNumber+1);
+    }
+    else{
+      setViolationNumber(violationNumber);
+    }
+
+
   };
   const loginPasswordHandler = (event) => {
+    if(showViolations===true){
+      setAutoHeight(autoHeight-violationNumber*7);
+    }
+    setViolationNumber(0);
+    setShowViolation(false)
     setEnteredLoginPassword(event.target.value);
+    if(event.target.value.length<8){
+      setLoginPasswordValidation(false);
+      setLoginPasswordValidationMsg("رمزعبور حداقل باید شامل 8 کاراکتر باشد")
+    }else{
+      let pattern=new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)" + "(?=.*[-+_!@#$%^&*., ?]).+$")
+      if(!pattern.test(event.target.value)){
+        setLoginPasswordValidationMsg("رمزعبور باید شامل حروف کوچک و بزرگ انگلیسی،اعداد و نشانه های خاص باشد")
+        setLoginPasswordValidation(false);
+      }else{
+        setLoginPasswordValidation(true);
+      }
+      
+    }
+    if(loginPasswordValidation===false){
+      setViolationNumber(violationNumber+1);
+    }
+    else{
+      setViolationNumber(violationNumber);
+    }
   };
+
+
+  //Register validation
+  //--------------------------------------------------------------------------------------------------
   const registerEmailHandler = (event) => {
+    if(showViolations===true){
+      setAutoHeight(autoHeight-violationNumber*7);
+    }
+    setViolationNumber(0);
+    setShowViolation(false)
     setEnteredRegisterEmail(event.target.value);
+    if (!String(event.target.value)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    )){
+      setRegisterEmailValidation(false);
+    }
+    else{
+      setRegisterEmailValidation(true);
+    }
+
+    if(registerEmailValidation===false){
+      setViolationNumber(violationNumber+1);
+    }
+    else{
+      setViolationNumber(violationNumber);
+    }
+
   };
+
   const registerPasswordHandler = (event) => {
+    if(showViolations===true){
+      setAutoHeight(autoHeight-violationNumber*7);
+    }
+    setViolationNumber(0);
+    setShowViolation(false)
     setEnteredRegisterPassword(event.target.value);
+    if(event.target.value.length<8){
+      setRegisterPasswordValidation(false);
+      setRegisterPasswordValidationMsg("رمزعبور حداقل باید شامل 8 کاراکتر باشد")
+    }else{
+      let pattern=new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)" + "(?=.*[-+_!@#$%^&*., ?]).+$")
+      if(!pattern.test(event.target.value)){
+        setRegisterPasswordValidationMsg("رمزعبور باید شامل حروف کوچک و بزرگ انگلیسی،اعداد و نشانه های خاص باشد")
+        setRegisterPasswordValidation(false);
+      }else{
+        setRegisterPasswordValidation(true);
+      }
+      
+    }
+    if(registerPasswordValidation===false){
+      setViolationNumber(violationNumber+1);
+    }
+    else{
+      setViolationNumber(violationNumber);
+    }
   };
   const registerPasswordHandler2 = (event) => {
+    if(showViolations===true){
+      setAutoHeight(autoHeight-violationNumber*7);
+    }
+    setViolationNumber(0);
+    setShowViolation(false)
     setEnteredRegisterPassword2(event.target.value);
+    if(event.target.value!==enteredRegisterPassword){
+      setRegisterPasswordValidation2(false);
+      setRegisterPasswordValidationMsg2("رمزعبور و تکرار آن باید یکسان باشند")
+    }else{
+      if(event.target.value.length<8){
+      setRegisterPasswordValidation2(false);
+      setRegisterPasswordValidationMsg2("رمزعبور حداقل باید شامل 8 کاراکتر باشد")
+    }else{
+      let pattern=new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)" + "(?=.*[-+_!@#$%^&*., ?]).+$")
+      if(!pattern.test(event.target.value)){
+        setRegisterPasswordValidationMsg2("رمزعبور باید شامل حروف کوچک و بزرگ انگلیسی،اعداد و نشانه های خاص باشد")
+        setRegisterPasswordValidation2(false);
+      }else{
+        setRegisterPasswordValidation2(true);
+      }
+      
+    }
+    }
+
+    if(registerPasswordValidation2===false){
+      setViolationNumber(violationNumber+1);
+    }
+    else{
+      setViolationNumber(violationNumber);
+    }
+    
   };
+
+
   const nameHandler = (event) => {
+    if(showViolations===true){
+      setAutoHeight(autoHeight-violationNumber*7);
+    }
+    setViolationNumber(0);
+    setShowViolation(false)
     setEnteredName(event.target.value);
+    if(event.target.value.length<5 || event.target.value.length>30){
+      setNameValidation(false);
+    }else{
+      setNameValidation(true);
+    }
+
+    if(nameValidation===false){
+      setViolationNumber(violationNumber+1);
+    }
+    else{
+      setViolationNumber(violationNumber);
+    }
   };
+
   const birthDateHandler = (event) => {
     setEnteredBirthDate(event.target.value);
   };
@@ -118,7 +317,7 @@ const Signup = () => {
           <div className="row full-height justify-content-center">
             <div className="col-12 text-center align-self-center py-5">
               <div className="section pb-5 pt-5 pt-sm-2 text-center">
-                <div className="card-3d-wrap mx-auto ">
+                <div className="card-3d-wrap mx-auto " style={{height: autoHeight.toString()+"px"}}>
                   {showLogin && (
                     <div className="card-front ">
                       <div className="center-wrap">
@@ -134,10 +333,11 @@ const Signup = () => {
                             />
                             <i className="input-icon uil uil-at"></i>
                           </div>
+                          {!loginEmailValidation && showViolations &&(<p className="mb-0 mt-2 validationMsg">فرمت ایمیل نادرست است</p>)}
                           <div className="form-group mt-2">
-                            <i class={showPassword ? "bi bi-eye":"bi bi-eye-slash"} onClick={togglePasswordVisibility} style={{ fontSize: "20px", position: "absolute", top: "40%", transform: "translateY(-50%)", paddingLeft: "10px"  }}></i>
+                            <i class={showLoginPassword ? "bi bi-eye":"bi bi-eye-slash"} onClick={toggleLoginPasswordVisibility} style={{ fontSize: "20px", position: "absolute", top: "40%", transform: "translateY(-50%)", paddingLeft: "10px"  }}></i>
                             <input
-                              type={showPassword ? "text":"password"}
+                              type={showLoginPassword ? "text":"password"}
                               className="form-style"
                               placeholder="رمز عبور"
                               value={enteredLoginPassword}
@@ -145,6 +345,7 @@ const Signup = () => {
                             />
                             <i className="input-icon uil uil-lock-alt" ></i>
                           </div>
+                          {!loginPasswordValidation && showViolations &&(<p className="mb-0 mt-2 validationMsg">{loginPasswordValidationMsg}</p>)}
                           <p className="mb-0 mt-2">
                             <a className="link" href="">بازیابی رمز عبور</a>
                           </p>
@@ -173,6 +374,11 @@ const Signup = () => {
                       </div>
                     </div>
                   )}
+
+
+
+
+
                   {!showLogin && (
                     <div className="card-back">
                       <div className="center-wrap">
@@ -188,6 +394,7 @@ const Signup = () => {
                             />
                             <i className="input-icon uil uil-user"></i>
                           </div>
+                          {!nameValidation && showViolations &&(<p className="mb-0 mt-2 validationMsg">نام و نام خانوادگی باید بین 5 تا 30 کاراکتر باشد</p>)}
                           <div className="form-group mt-2">
                             <input
                               type="email"
@@ -198,10 +405,11 @@ const Signup = () => {
                             />
                             <i className="input-icon uil uil-at"></i>
                           </div>
+                          {!registerEmailValidation && showViolations &&(<p className="mb-0 mt-2 validationMsg">فرمت ایمیل نادرست است</p>)}
                           <div className="form-group mt-2">
-                            <i class={showPassword ? "bi bi-eye":"bi bi-eye-slash"} onClick={togglePasswordVisibility} style={{ fontSize: "20px", position: "absolute", top: "40%", transform: "translateY(-50%)", paddingLeft: "10px"  }}></i>
+                            <i class={showRegisterPassword ? "bi bi-eye":"bi bi-eye-slash"} onClick={toggleRegisterPasswordVisibility} style={{ fontSize: "20px", position: "absolute", top: "40%", transform: "translateY(-50%)", paddingLeft: "10px"  }}></i>
                             <input
-                              type={showPassword ? "text":"password"}
+                              type={showRegisterPassword ? "text":"password"}
                               className="form-style"
                               placeholder="رمز عبور"
                               value={enteredRegisterPassword}
@@ -209,10 +417,11 @@ const Signup = () => {
                             />
                             <i className="input-icon uil uil-lock-alt"></i>
                           </div>
+                          {!registerPasswordValidation && showViolations &&(<p className="mb-0 mt-2 validationMsg">{registerPasswordValidationMsg}</p>)}
                           <div className="form-group mt-2">
-                          <i class={showPassword2 ? "bi bi-eye":"bi bi-eye-slash"} onClick={togglePasswordVisibility2} style={{ fontSize: "20px", position: "absolute", top: "40%", transform: "translateY(-50%)", paddingLeft: "10px"  }}></i>
+                          <i class={showRegisterPassword2 ? "bi bi-eye":"bi bi-eye-slash"} onClick={toggleRegisterPasswordVisibility2} style={{ fontSize: "20px", position: "absolute", top: "40%", transform: "translateY(-50%)", paddingLeft: "10px"  }}></i>
                             <input
-                              type={showPassword2 ? "text":"password"}
+                              type={showRegisterPassword2 ? "text":"password"}
                               className="form-style"
                               placeholder="تایید رمز عبور"
                               value={enteredRegisterPassword2}
@@ -220,6 +429,7 @@ const Signup = () => {
                             />
                             <i className="input-icon uil uil-lock-alt"></i>
                           </div>
+                          {!registerPasswordValidation2 && showViolations &&(<p className="mb-0 mt-2 validationMsg">{registerPasswordValidationMsg2}</p>)}
                           <div className="form-group mt-2">
                             <input
                               type="text"
