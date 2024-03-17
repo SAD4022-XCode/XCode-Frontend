c8a898f0503a0cab8ab7e04654d561210d370aa5
Alibest3 <alibest3@yahoo.com>
2024-03-17 12:07:40 +0330

    google login button designed

--- src/Components/Login-Register/google.js
+++ src/Components/Login-Register/google.js
@@ -1,8 +1,7 @@
 import {GoogleLogin} from 'react-google-login';
-
 const clientID = "191069690020-bfq8g99fjkeskb60o0rqjri7cecm6r9l.apps.googleusercontent.com";
 
-function Login(){
+function GoogleButton(){
     const onSuccess = (res) =>{
         console.log("Login Success! Current User: ",res.profileObj);
     }
@@ -12,17 +11,31 @@ function Login(){
     }
 
     return (
-        <div id="googleButton">
+        <div>
             <GoogleLogin
                 clientId={clientID}
-                buttonText='Login with Google'
+                // buttonText='Login with Google' 
                 onSuccess={onSuccess}
                 onFailure={onFailure}
                 cookiePolicy={'single_host_origin'}
-                isSignedIn={true}            
-            />
-        </div>
+                isSignedIn={true} 
+                className="google-login-button" 
+                icon={false}
+                // redirectUri
+            >
+                <div class="container" style={{ height: '25px'}}>
+                    <div class="row">
+                        <div>
+                          <img 
+                            src={require("../../assets/google-logo.png")}
+                            style={{ width: '25px', height: '25px',marginBottom:'5px'}}/>
+                        </div>
+                        <h6 style={{ marginTop: '2px',marginInlineStart: '5px'}}>  ورود با گوگل </h6>
+                    </div> 
+                </div>
+            </GoogleLogin>
+        </div> 
     )
 }
-
-export default Login;
+ 
+export default GoogleButton;
