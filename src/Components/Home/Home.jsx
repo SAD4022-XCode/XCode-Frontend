f0aaac94be88fd76f9818f583d62e5910f68410c
Sajjad <13.sajjad.mehrpeyma.81@gmail.com>
2024-04-19 14:57:01 +0330

    feat/5 recent event Slider added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import Navbar  from "../Navbar/navbar";
 import {useNavigate} from 'react-router-dom';
 import './Home.css'
+import UpcomingEvents from "./upcoming_events/upcoming_events";
+
 const Home = () => {
     const navigator=useNavigate();
    
@@ -12,8 +14,8 @@ const Home = () => {
     return (
         <center>
             <Navbar />
-            <h1 style={{paddingTop: "250px"}}>Home Page</h1>
-            
+            {/* <h1 style={{paddingTop: "250px"}}>Home Page</h1> */}
+            <UpcomingEvents/>
         </center>
     )
 
