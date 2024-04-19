93655fd4f9c4b675f5e677a473a76a5338cb51f4
Alibest3 <alibest3@yahoo.com>
2024-04-19 15:41:56 +0330

    fix(Home): fix scroll and add Events List

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Navbar  from "../Navbar/navbar";
 import {useNavigate} from 'react-router-dom';
 import UpcomingEvents from "./UpcomingEvents/upcomingEvents";
+import Events from "../Events List/Events";
 import './Home.css'
 const Home = () => {
     const navigator=useNavigate();
@@ -14,6 +15,7 @@ const Home = () => {
         <center>
             <Navbar />
             <UpcomingEvents/>
+            <Events />
             {/* <h1 style={{paddingTop: "250px"}}>Home Page</h1> */}
             
         </center>
