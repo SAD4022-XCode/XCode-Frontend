fa6278d25cafa88246798fefc42eddcbffb4485a
Alibest3 <alibest3@yahoo.com>
2024-05-02 20:57:13 +0330

    feat(EventsList): filter connected to back-end

--- src/Components/Events List/EventsFilter.jsx
+++ src/Components/Events List/EventsFilter.jsx
@@ -16,40 +16,54 @@ const EventsFilter = ({ sendFilteredPosts }) => {
 
   const handleEventStartDate = (e) => {
     setEventStartDate(e);
-    debouncedSendData({ startDate: e.target.value });
+    // debouncedSendData({ startDate: e.target.value });
   };
   const handleEventEndDate = (e) => {
     setEventEndDate(e);
-    debouncedSendData({ endDate: e.target.value });
+    // debouncedSendData({ endDate: e.target.value });
   };
   const handleEventCategory = (e) => {
     setEventCategory(selectedTags);
-    debouncedSendData({ category: selectedTags });
+    // debouncedSendData({ category: selectedTags });
   };
 
   const handleEventType = (e) => {
     setEventType(e.target.value);
-    debouncedSendData({ type: e.target.value });
+    // debouncedSendData({ type: e.target.value });
   };
 
   const handleEventPrice = (e) => {
     setEventPrice(e.target.value);
-    debouncedSendData({ price: e.target.value });
+    // debouncedSendData({ price: e.target.value });
   };
+  useEffect(() => {
+    let data = {
+      eventPrice: eventPrice,
+      eventType: eventType,
+      eventStartDate: eventStartDate,
+      eventEndDate: eventEndDate,
+      selectedTags: selectedTags,
+    };
 
-  const debouncedSendData = useCallback(
-    debounce((data) => {
-      axios
-        .post("https://api.yourbackend.com/events", data)
-        .then((response) => {
-          console.log("Data sent successfully:", response.data);
-        })
-        .catch((error) => {
-          console.error("Failed to send data:", error);
-        });
-    }, 500),
-    []
-  ); // 500ms delay; // 500ms delay
+    const getData = setTimeout(() => {
+      sendFilteredPosts(data);
+      // console.log(data);
+    }, 800);
+    return () => clearTimeout(getData);
+  }, [eventPrice, eventType, eventStartDate, eventEndDate, selectedTags]);
+  // const debouncedSendData = useCallback(
+  //   debounce((data) => {
+  //     axios
+  //       .post("https://api.yourbackend.com/events", data)
+  //       .then((response) => {
+  //         console.log("Data sent successfully:", response.data);
+  //       })
+  //       .catch((error) => {
+  //         console.error("Failed to send data:", error);
+  //       });
+  //   }, 500),
+  //   []
+  // ); // 500ms delay; // 500ms delay
   // axios.defaults.headers.common["X-Jsio-Token"] =
   //   "69b3f5f4d98b76f3d1337f262baeefbf";
   // useEffect(() => {
@@ -66,11 +80,11 @@ const EventsFilter = ({ sendFilteredPosts }) => {
   //   };
   //   fetchEvents();
   // }, []);
-  useEffect(() => {
-    return () => {
-      debouncedSendData.cancel();
-    };
-  }, []);
+  // useEffect(() => {
+  //   return () => {
+  //     debouncedSendData.cancel();
+  //   };
+  // }, []);
   return (
     <center>
       <div className="event-filter">
@@ -123,8 +137,8 @@ const EventsFilter = ({ sendFilteredPosts }) => {
                 <label>قیمت</label>
                 <select onChange={handleEventPrice}>
                   <option value="">همه رویدادها</option>
-                  <option value="F">رایگان</option>
-                  <option value="P">غیر رایگان</option>
+                  <option value="False">رایگان</option>
+                  <option value="True">غیر رایگان</option>
                 </select>
               </div>
             </div>
