641f6cc8fa63ec5173629be58e6a77a0b8964b9d
Alibest3 <alibest3@yahoo.com>
2024-05-17 02:13:03 +0330

    feat(Comments): comments send data to back

--- src/Components/EventDetails/Comment/DeleteModal.js
+++ src/Components/EventDetails/Comment/DeleteModal.js
@@ -1,11 +1,20 @@
 import './deleteModal.css';
-
+import axios from "axios";
+import { useAuth } from "../../Authentication/authProvider";
 const DeleteModal = ({
     id,
     setDeleteComment,
     setData,
     data
 }) => {
+    const auth = useAuth();
+    // const deleteComment = async (id) => {
+    //     const baseUrl = `https://eventify.liara.run/comments/${id}/`;
+    //     console.log(auth.token);
+    //     await axios.delete(baseUrl, {headers: {
+    //       Authorization: `JWT ${auth.token}`,
+    //     }})
+    //   };
     const deleteComment = () => {
             for (let comment of data) {
                 if (comment.id === id) {
@@ -17,8 +26,8 @@ const DeleteModal = ({
                 }
                 if (comment.replies.length > 0) {
                     for (let reply of comment.replies) {
-                        if (reply.parent === id) {
-                            const updatedReplies = comment.replies.filter((reply) => reply.parent !== id);
+                        if (reply.id === id) {
+                            const updatedReplies = comment.replies.filter((reply) => reply.id !== id);
 
                             comment.replies = updatedReplies;
                             setData(data);
