778b7d279584a16356d3e946de96b5b4005146ca
Sajjad <13.sajjad.mehrpeyma.81@gmail.com>
2024-05-26 20:53:30 +0330

    fix/paste from clipboard ui added

--- src/Components/EventDetails/organizer-contact-info.jsx
+++ src/Components/EventDetails/organizer-contact-info.jsx
@@ -26,13 +26,18 @@ const OrganizerInfoModal = ({show,handleClose,email,phone}) => {
         setInput(input + emoji);
       };
 
-    const showEmojiPicker = () => {
-        return(
-            // <div>
-                <Picker onSelect={addEmoji} />
-            // </div>
-        )
-    }
+    // const showEmojiPicker = () => {
+    //     return(
+    //             <Picker onSelect={addEmoji} />
+    //     )
+    // }
+
+      const pasteClipboard = (e) =>{
+        // const pastedText = await navigator.clipboard.read();
+        setInput(pastedText)    
+
+      }
+
     
     const sendMessageHandler = () =>{
         toast.success('پیام شما برای برگزارکننده ارسال شد', {
@@ -73,22 +78,46 @@ const OrganizerInfoModal = ({show,handleClose,email,phone}) => {
         <div id="chat">
             <div class="chat__conversation-panel">
                 <div class="chat__conversation-panel__container">
-                    {/* <button class="chat__conversation-panel__button panel-item btn-icon add-file-button">
-                        <svg class="feather feather-plus sc-dnqmqq jxshSx" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
+                    {/* {<button class="chat__conversation-panel__button panel-item btn-icon add-file-button">
+                        <svg class="feather feather-plus sc-dnqmqq jxshSx" 
+                             xmlns="http://www.w3.org/2000/svg"
+                             width="24" 
+                             height="24" 
+                             viewBox="0 0 24 24" 
+                             fill="none" 
+                             stroke="currentColor" 
+                             stroke-width="2" 
+                             stroke-linecap="round" 
+                             stroke-linejoin="round" 
+                             aria-hidden="true">
                             <line x1="12" y1="5" x2="12" y2="19"></line>
                             <line x1="5" y1="12" x2="19" y2="12"></line>
                             </svg>
-                        </button> */}
+                        </button>}    */}
 
-                    {/* <button class="chat__conversation-panel__button panel-item btn-icon emoji-button" 
-                            onClick={() => setShowEmojis(!showEmojis)}>
-                        <svg class="feather feather-smile sc-dnqmqq jxshSx" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
+                    {<button class="chat__conversation-panel__button panel-item btn-icon emoji-button" 
+                            onClick={pasteClipboard}>
+                        
+                        <i class="bi bi-clipboard"></i>
+                        {/* <svg 
+                            //  class="feather feather-smile sc-dnqmqq jxshSx" 
+                             class="bi bi-clipboard"
+                             xmlns="http://www.w3.org/2000/svg" 
+                             width="24" 
+                             height="24" 
+                             viewBox="0 0 24 24" 
+                             fill="none" 
+                             stroke="currentColor" 
+                             stroke-width="2" 
+                             stroke-linecap="round" 
+                             stroke-linejoin="round" 
+                             aria-hidden="true">
                             <circle cx="12" cy="12" r="10"></circle>
                             <path d="M8 14s1.5 2 4 2 4-2 4-2"></path>
                             <line x1="9" y1="9" x2="9.01" y2="9"></line>
                             <line x1="15" y1="9" x2="15.01" y2="9"></line>
-                        </svg>
-                    </button> */}
+                        </svg> */}
+                    </button>}
                     <input class="chat__conversation-panel__input panel-item" 
                            id="chat__conversation-panel__input"
                            placeholder={placeHolder}
@@ -101,7 +130,17 @@ const OrganizerInfoModal = ({show,handleClose,email,phone}) => {
                     <button class="chat__conversation-panel__button panel-item btn-icon send-message-button" 
                     onClick={sendMessageHandler}
                     disabled={isInputDisabled}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true" data-reactid="1036">
+                        <svg xmlns="http://www.w3.org/2000/svg" 
+                             width="24" 
+                             height="24" 
+                             viewBox="0 0 24 24" 
+                             fill="none" 
+                             stroke="currentColor" 
+                             stroke-width="2" 
+                             stroke-linecap="round" 
+                             stroke-linejoin="round" 
+                             aria-hidden="true" 
+                             data-reactid="1036">
                             <line x1="22" y1="2" x2="11" y2="13"></line>
                             <polygon points="22 2 15 22 11 13 2 9 22 2"></polygon>
                         </svg>
