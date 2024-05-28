38181f5d6264dba559e868e7ac24cf9143516a97
Alibest3 <alibest3@yahoo.com>
2024-05-28 22:35:54 +0330

    feat(NotificationPanel): title and date added

--- src/Components/NotificationPanel/NotificationPanel.jsx
+++ src/Components/NotificationPanel/NotificationPanel.jsx
@@ -5,20 +5,48 @@ import { Modal } from "react-bootstrap";
 import Lottie from "react-lottie";
 import animationData from "./Animation - 1715616866327.json";
 import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
+import moment from "moment-timezone";
 
 const NotificationPanel = () => {
   const [notifications, setNotifications] = useState([
-    { id: 1, message: "ایونت شماره 1", read: false },
-    { id: 2, message: "ایونت شماره 2", read: false },
-    { id: 3, message: "ایونت شماره 3", read: true },
-    { id: 4, message: "ایونت شماره 4", read: true },
+    {
+      id: 1,
+      recipient: "",
+      title: "ایونت شماره 1 ایجاد شد",
+      content: "ایونت شماره 1",
+      time: "2024-05-28T16:54:37.465Z",
+      is_read: false,
+    },
+    {
+      id: 2,
+      recipient: "",
+      title: "ایونت شماره 2 ایجاد شد",
+      content: "ایونت شماره 2",
+      time: "2024-05-28T16:54:37.465Z",
+      is_read: false,
+    },
+    {
+      id: 3,
+      recipient: "",
+      title: "ایونت شماره 3 ایجاد شد",
+      content: "ایونت شماره 3",
+      time: "2024-05-28T16:54:37.465Z",
+      is_read: true,
+    },
+    {
+      id: 4,
+      recipient: "",
+      title: "ایونت شماره 4 ایجاد شد",
+      content: "ایونت شماره 4",
+      time: "2024-05-28T16:54:37.465Z",
+      is_read: true,
+    },
   ]);
   const [showAll, setShowAll] = useState(false);
   const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [loading, setLoading] = useState(false);
   let count = 0;
   notifications.map((notification) => {
-    !notification.read ? count++ : (count = count);
+    !notification.is_read ? count++ : (count = count);
   });
   const [notificationsCount, setNotificationsCount] = useState(count);
   const defaultOptions = {
@@ -39,12 +67,42 @@ const NotificationPanel = () => {
   const markAllAsRead = () => {
     const updatedNotifications = notifications.map((notification) => ({
       ...notification,
-      read: true,
+      is_read: true,
     }));
     setNotifications(updatedNotifications);
     setNotificationsCount(0);
   };
+  const date = new Date().toISOString();
+  const translateTime = (time) => {
+    let translatedTime = moment.utc(time).local().fromNow();
+    const translations = {
+      ago: "قبل",
+      "a few seconds": "لحظاتی",
+      days: "روز",
+      a: "یک",
+      day: "روز",
+      month: "ماه",
+      months: "ماه",
+      year: "سال",
+      years: "سال",
+      week: "هفته",
+      weeks: "هفته",
+      minutes: "دقیقه",
+      minute: "دقیقه",
+      hours: "ساعت",
+      hour: "ساعت",
+      seconds: "ثانیه",
+      few: "چند",
+    };
 
+    for (let key in translations) {
+      if (translatedTime.includes(key)) {
+        translatedTime = translatedTime.replace(key, translations[key]);
+      }
+    }
+
+    return translatedTime;
+  };
   return (
     <>
       <div className="lottie-parent" onClick={toggleModal}>
@@ -77,30 +135,44 @@ const NotificationPanel = () => {
             نمایش همه اعلان ها
             <Switch checked={showAll} onChange={toggleShowAll} />
           </label>
-          <p onClick={markAllAsRead}>خواندن همه</p>
+          <p id="read-all" onClick={markAllAsRead}>
+            خواندن همه
+          </p>
           <ul>
             {notifications
-              .filter((notification) => showAll || !notification.read)
+              .filter((notification) => showAll || !notification.is_read)
               .map((notification) => (
                 <li
                   key={notification.id}
-                  className={notification.read ? "read" : "unread"}
+                  className={notification.is_read ? "is_read" : "unread"}
                   onClick={() => {
                     const updatedNotifications = notifications.map((notif) => {
                       if (notif.id === notification.id) {
-                        return { ...notif, read: true };
+                        return { ...notif, is_read: true };
                       }
                       return notif;
                     });
                     setNotifications(updatedNotifications);
                     count = 0;
                     updatedNotifications.map((notification) => {
-                      !notification.read ? count++ : (count = count);
+                      !notification.is_read ? count++ : (count = count);
                     });
                     setNotificationsCount(count);
                   }}
                 >
-                  {notification.message}
+                  <div className="title">
+                    {!notification.is_read && (
+                      <span className="blue-bullet"></span>
+                    )}
+                    <p id="notification-title">{notification.title}</p>
+                    {/* place bullet here */}
+                  </div>
+                  <div className="content">
+                    <p id="notification-content">{notification.content}</p>
+                    <p id="notification-time">
+                      {translateTime(notification.time)}
+                    </p>
+                  </div>
                 </li>
               ))}
           </ul>
